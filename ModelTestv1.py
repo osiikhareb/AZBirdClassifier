384cb523e91be61d48274bedaa071f3ec9b19665
Osi Ikharebha <61285142+osiikhareb@users.noreply.github.com>
2024-01-26 22:18:29 -0700

    Add files via upload

---
+++ ModelTestv1.py
@@ -0,0 +1,98 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Jan 21 21:54:49 2024
+
+@author: Osi
+"""
+
+import os, shutil, pathlib
+import pandas as pd
+import numpy as np
+import tensorflow as tf
+from tensorflow import keras
+from tensorflow import data as tf_data
+from tensorflow.keras import layers, models
+from tensorflow.keras.utils import image_dataset_from_directory
+import matplotlib.pyplot as plt
+
+
+
+
+# Instantiate model
+inputs = keras.Input(shape=(480, 480, 3))
+x = layers.Rescaling(1./255)(inputs)
+x = layers.Conv2D(filters=32, kernel_size=3, activation="relu")(x)
+x = layers.MaxPooling2D(pool_size=2)(x)
+x = layers.Conv2D(filters=64, kernel_size=3, activation="relu")(x)
+x = layers.MaxPooling2D(pool_size=2)(x)
+x = layers.Conv2D(filters=128, kernel_size=3, activation="relu")(x)
+x = layers.MaxPooling2D(pool_size=2)(x)
+x = layers.Conv2D(filters=256, kernel_size=3, activation="relu")(x)
+x = layers.MaxPooling2D(pool_size=2)(x)
+x = layers.Conv2D(filters=256, kernel_size=3, activation="relu")(x)
+x = layers.Flatten()(x)
+outputs = layers.Dense(1, activation="sigmoid")(x)
+model = keras.Model(inputs=inputs, outputs=outputs)
+
+# observe model
+model.summary()
+
+
+# Configure model for training
+model.compile(loss="binary_crossentropy",
+              optimizer="rmsprop",
+              metrics=["accuracy"])
+
+
+new_base_dir = pathlib.Path("A:\Documents\Python Scripts\BirdBot2.0\Training\_images")
+
+# Import training, validation, and test data
+train_dataset = image_dataset_from_directory(
+    new_base_dir / "train",
+    image_size=(480, 480),
+    batch_size=32)
+validation_dataset = image_dataset_from_directory(
+    new_base_dir / "validation",
+    image_size=(480, 480),
+    batch_size=32)
+test_dataset = image_dataset_from_directory(
+    new_base_dir / "test",
+    image_size=(480, 480),
+    batch_size=32)
+
+
+# Fit model to training data
+callbacks = [
+    keras.callbacks.ModelCheckpoint(
+        filepath="A:\Documents\Python Scripts\BirdBot2.0\Training",
+        save_best_only=True,
+        monitor="val_loss")
+]
+history = model.fit(
+    train_dataset,
+    epochs=30,
+    validation_data=validation_dataset,
+    callbacks=callbacks)
+
+
+# Visiualize model accuracy
+accuracy = history.history["accuracy"]
+val_accuracy = history.history["val_accuracy"]
+loss = history.history["loss"]
+val_loss = history.history["val_loss"]
+epochs = range(1, len(accuracy) + 1)
+plt.plot(epochs, accuracy, "bo", label="Training accuracy")
+plt.plot(epochs, val_accuracy, "b", label="Validation accuracy")
+plt.title("Training and validation accuracy")
+plt.legend()
+plt.figure()
+plt.plot(epochs, loss, "bo", label="Training loss")
+plt.plot(epochs, val_loss, "b", label="Validation loss")
+plt.title("Training and validation loss")
+plt.legend()
+plt.show()
+
+# Test model on test dataset
+test_model = keras.models.load_model("A:\Documents\Python Scripts\BirdBot2.0\Training")
+test_loss, test_acc = test_model.evaluate(test_dataset)
+print(f"Test accuracy: {test_acc:.3f}")
